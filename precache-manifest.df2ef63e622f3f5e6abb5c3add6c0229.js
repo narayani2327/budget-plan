5e97c3889e529c66193df53db0da753afc524a9a
narayani2327 <harikalanarayani@gmail.com>
2023-02-18 09:22:02 +0530

    Updates

---
+++ precache-manifest.df2ef63e622f3f5e6abb5c3add6c0229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15d401a93784bbd8c225c7378e52743",
+    "url": "/budget-plan/index.html"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/budget-plan/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2633210a928497d44cbc",
+    "url": "/budget-plan/static/js/2.360a4eba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-plan/static/js/2.360a4eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d041730df3aaad259846",
+    "url": "/budget-plan/static/js/main.25c9e7e2.chunk.js"
+  },
+  {
+    "revision": "d2a3f42a8feee6fead51",
+    "url": "/budget-plan/static/js/runtime-main.a00aaa1b.js"
+  }
+]);
